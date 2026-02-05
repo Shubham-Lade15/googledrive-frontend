ad9f3467449d5833752fa74b8a3ee64fa68d7704
Shubham <shubhamlade495@gmail.com>
2026-02-05 23:00:15 +0530

    Ready for deployment

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://googledrive-backend-xd4z.onrender.com"
+  baseURL: "https://googledrive-backend-xd4z.onrender.com/api"
 });
 
 // attach JWT token to every request

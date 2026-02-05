88acc9a9bfe0fb1099800c39f810928ac2f70f3d
Shubham <shubhamlade495@gmail.com>
2026-02-05 22:37:40 +0530

    Fixed: email activation

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://googledrive-backend-xd4z.onrender.com/api"
+  baseURL: "http://localhost:5000/api"
 });
 
 // attach JWT token to every request
